b5d3ebd30c89bf112d3ecdd7b657eb507053984a
Diya Joshy <diyajoshy8ackmnss@gmail.com>
2026-01-18 18:48:59 +0530

    Adding extractin feature

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, render_template
+import pymupdf
+
+
 app = Flask(__name__)
-# decorator calls age() when / is called
+# decorator calls recieve() when / is called
 @app.route('/',methods=["GET","POST"])
-def age():
+def recieve():
     if request.method == "POST":
-        file = request.files["pdf"]
+        file = request.files["pdf"]     #stores an object
+        byte_form=file.read()        #reads the file in byte form from the object
+
+        doc = pymupdf.open(stream=byte_form,filetype="pdf")     #opens the file from the byte form
+        for page in doc:
+            text=page.get_text()                                  #converts byte to human readable stuff
+            print(text)
         return "file successfully recieved"
     else:
-        return "error occured"
+        return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True)
