bf808335e94e9fe2f12a8602ede15351e11d756f
Thes3us <171039607+Thes3us@users.noreply.github.com>
2026-01-18 19:59:48 +0530

    added parse status to html, handled success redirect

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import pymupdf
 
 
 app = Flask(__name__)
-# decorator calls recieve() when / is called
+# decorator calls receive() when / is called
 @app.route('/',methods=["GET","POST"])
-def recieve():
+def receive():
+    #handle submit button
     if request.method == "POST":
-        file = request.files["pdf"]     #stores an object
+        file = request.files["pdf"]   #stores pdf
         byte_form=file.read()        #reads the file in byte form from the object
-
         doc = pymupdf.open(stream=byte_form,filetype="pdf")     #opens the file from the byte form
+
         for page in doc:
-            text=page.get_text()                                  #converts byte to human readable stuff
+            text=page.get_text()   #converts byte to human readable stuff
             print(text)
-        return "file successfully recieved"
+        #sends user to a new page with parameter "success" to avoid looping bugs
+        return redirect(url_for("receive", status="success")) 
+    
+    status = request.args.get("status") #receives status from URL 
+
+    if status == "success":
+        message = "file successfully received"
     else:
-        return render_template("index.html")
+        #handles message when website is visited without submit
+        message = None
+    return render_template("index.html", message = message)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) #tset
