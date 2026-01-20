204e63cd4d57df596274a2839b915fce44f6469a
Thes3us <171039607+Thes3us@users.noreply.github.com>
2026-01-20 21:41:10 +0530

    return content back to app.py

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,19 +1,18 @@
 import pymupdf
 def send(file):
-    """sends pdf file from Flask app to be process
+    """sends pdf file from Flask app to be processed
     Args:
         file (bytestream): The pdf in bytestream
     Returns:
         boolean: Whether if data exists or not
     """
     if not file:
-        return False
-
+        return False # if file is empty
     byte_form=file.read()        #reads the file in byte form from the object
     doc = pymupdf.open(stream=byte_form,filetype="pdf")     #opens the file from the byte form
     content = []
     for page in doc:
         text=page.get_text()   #converts byte to human readable stuff
-        content.append(text)
+        content.append(text)   #append each page into text
     print(content)
-    return True
+    return(content, True) # return content and hascontent
